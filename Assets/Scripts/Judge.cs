077cd1fedce77964463fe6e64cc38aa543ae9ac2
175atsu <atsu@iinoatsushihakunoMacBook-Air.local>
2017-11-12 02:54:10 +0900

    sa

---
+++ Assets/Scripts/Judge.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Judge : MonoBehaviour {
+	public GameObject parent;
+
+
+
+	public void OnClick (int number){
+
+		switch (number) {
+
+		case 0:
+			Debug.Log("不正解");
+			parent.SetActive(false);
+			break;
+		case 1:
+			Debug.Log ("正解");
+			parent.SetActive(false);
+			break;
+		case 2:
+			Debug.Log("不正解");
+			parent.SetActive(false);
+			break;
+		default:
+			break;
+		}
+
+	}
+
+
+//	void Update(){
+//		if (Input.GetMouseButtonDown (0)) {
+//			JudgeAnswer ();
+//		} else {
+//		}
+//	}
+//
+//	//選択したボタンのテキストラベルと正解のテキストを比較して正誤を判定
+//	public void JudgeAnswer(){
+//		//正解のデータをテキストでセットする
+//		string answerText = "(2) 黄色";
+//		//選択したボタンのテキストラベルを取得する
+//		Text selectedBtn = this.GetComponentInChildren<Text> ();
+//
+//		if (selectedBtn.text == answerText) {
+//			Debug.Log("正解");
+//		} else {
+//			Debug.Log("不正解");
+//		}
+//	}
+}
